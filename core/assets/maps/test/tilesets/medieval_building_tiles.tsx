db6d5d06bd629c9e92ae3fafdf5e8da26ad0d62f
BWest <bwest@kth.se>
2017-09-19 00:48:02 +0200

    Added a testmap with tilesets

---
+++ core/assets/maps/test/tilesets/medieval_building_tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="medieval_building_tiles" tilewidth="64" tileheight="64" tilecount="192" columns="16">
+ <image source="medieval_building_tiles.png" width="1024" height="768"/>
+</tileset>
---
+++ core/assets/maps/test/tilesets/tileset_cave.tsx
@@ -0,0 +1,40 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="tileset_cave" tilewidth="64" tileheight="64" tilecount="48" columns="16">
+ <tileoffset x="0" y="16"/>
+ <grid orientation="isometric" width="64" height="32"/>
+ <image source="tileset_cave.png" width="1024" height="192"/>
+ <terraintypes>
+  <terrain name="Water" tile="40"/>
+  <terrain name="Ground" tile="0"/>
+ </terraintypes>
+ <tile id="0" terrain="1,1,1,1"/>
+ <tile id="1" terrain="1,1,1,1"/>
+ <tile id="2" terrain="1,1,1,1"/>
+ <tile id="3" terrain="1,1,1,1"/>
+ <tile id="16" terrain="1,0,1,0"/>
+ <tile id="17" terrain="1,1,0,0"/>
+ <tile id="18" terrain="0,1,0,1"/>
+ <tile id="19" terrain="0,0,1,1"/>
+ <tile id="20" terrain="1,0,1,0"/>
+ <tile id="21" terrain="1,1,0,0"/>
+ <tile id="22" terrain="0,1,0,1"/>
+ <tile id="23" terrain="0,0,1,1"/>
+ <tile id="24" terrain="1,0,1,1"/>
+ <tile id="25" terrain="1,1,1,0"/>
+ <tile id="26" terrain="1,1,0,1"/>
+ <tile id="27" terrain="0,1,1,1"/>
+ <tile id="28" terrain="1,0,1,1"/>
+ <tile id="29" terrain="1,1,1,0"/>
+ <tile id="30" terrain="1,1,0,1"/>
+ <tile id="31" terrain="0,1,1,1"/>
+ <tile id="32" terrain="0,0,1,0"/>
+ <tile id="33" terrain="1,0,0,0"/>
+ <tile id="34" terrain="0,1,0,0"/>
+ <tile id="35" terrain="0,0,0,1"/>
+ <tile id="36" terrain="0,0,1,0"/>
+ <tile id="37" terrain="1,0,0,0"/>
+ <tile id="38" terrain="0,1,0,0"/>
+ <tile id="39" terrain="0,0,0,1"/>
+ <tile id="40" terrain="0,0,0,0"/>
+ <tile id="41" terrain="0,0,0,0"/>
+</tileset>
